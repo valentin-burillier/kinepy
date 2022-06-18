c228feeed52e67dc5b083c40d372679a1bad07f0
valentin-burillier <93446869+valentin-burillier@users.noreply.github.com>
2022-06-18 15:37:54 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'kinepy',
+    packages = ['kinepy'],
+    version = '2.1.3',
+    description = 'Python library dedicated to plane mechanism simulation',
+    author = '',
+    author_email = '',
+    url = 'https://github.com/valentin-burillier/kinepy',
+    keywords = [],
+    requires=[],
+)
