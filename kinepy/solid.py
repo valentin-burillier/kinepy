ca4417e764d72dd224e1acb586a8a822844dba89
EKnight-Blue <loicchechevalier@gmail.com>
2022-07-20 11:26:16 +0200

    Ajout de j, m, g, alpha -> a

--- kinepy/solid.py
+++ kinepy/solid.py
@@ -4,20 +4,26 @@ import numpy as np
 class Solid:
     nb = 0
     
-    def __init__(self, points=(), named_points=None, name=''):
+    def __init__(self, points=(), named_points=None, j=0., m=0., g=(0., 0.), name=''):
         self.__class__.nb += 1
         self.points = list(points)
         self.name = name if name else f'Solid{Solid.nb}'
         self.named_points = named_points if named_points else dict()
         self.angle = self._points = self.origin = None
+        self.j, self.m, self.g = j, m, g
     
     def __setitem__(self, item, value):
         if isinstance(item, int):
             self.points[item] = value
         elif isinstance(item, str):
             if item not in self.named_points:
-                self.named_points[item] = len(self.points)
-                self.points.append(value)
+                if item == 'G':
+                    if not isinstance(value, tuple):
+                        raise TypeError('G is point that is not related to others, expected tuple')
+                    self.g = value
+                else:
+                    self.named_points[item] = len(self.points)
+                    self.points.append(value)
             else:
                 self.points[self.named_points[item]] = value
         else:
@@ -27,6 +33,8 @@ class Solid:
         if isinstance(item, int):
             return self.points[item]
         elif isinstance(item, str):
+            if item == 'G':
+                return self.g
             return self.points[self.named_points[item]]
     
     def reset(self, n):
@@ -34,7 +42,7 @@ class Solid:
         self.angle = np.zeros((n,), float)
 
     def _save(self):
-        return {'name': self.name, 'points': self.points, 'named_points': self.named_points}
+        return {'name': self.name, 'points': self.points, 'named_points': self.named_points, 'j': self.j, 'm': self.m, 'g': self.g}
     
     @classmethod
     def _load(cls, data):
