243f3d9761d8663d73cad44e3ced393662e44f6d
Burillier Valentin <93446869+valentin-burillier@users.noreply.github.com>
2022-07-27 20:26:59 +0200

    Update tools.py

--- kinepy/tools.py
+++ kinepy/tools.py
@@ -4,26 +4,31 @@ from matplotlib import animation
 from matplotlib import cm
 import matplotlib.colors as mcol
 
-
 global C
 # C = cm.get_cmap('coolwarm') # plasma, coolwarm, bwr, rainbow, OrRd
 C = mcol.LinearSegmentedColormap.from_list("", ["cyan", "r"])
 
-
 def animate(list_paths, list_vectors=None, anim_time=4, repeat=True, scale=1, vector_scale=0.1, magnitude_vector='proportionnal'):
-    # magnitude vector = {proportionnal, log, unitary}
-    M = np.array(list_paths) # prendre en compte chemins de diff taille
-    if len(M.shape) == 3:
-        M = M.reshape((1,) + M.shape)
-        
-    M = np.transpose(M, (0, 3, 2, 1))
-    nan = np.any(np.isnan(M), axis=(0, 2, 3))
-    M[:, nan] = 0
+    """
+    magnitude_vector : 'proportionnal', 'log', 'unitary'
 
-    x_min = M[:, :, 0].min()
-    x_max = M[:, :, 0].max()
-    y_min = M[: ,:, 1].min()
-    y_max = M[:, :, 1].max()
+    """
+    if isinstance(list_paths[0], np.ndarray):
+        list_paths = [list_paths]
+        
+    M = []
+    x_min, x_max, y_min, y_max = [], [], [], []
+    for path in list_paths:
+        A = np.array(path)
+        nan = np.any(np.isnan(A), axis=(1, 0))
+        A[:, :, nan] = 0
+        M.append(A.transpose((2, 1, 0)))
+        x_min.append(A[:, 0].min())
+        x_max.append(A[:, 0].max())
+        y_min.append(A[:, 1].min())
+        y_max.append(A[:, 1].max())
+
+    x_min, x_max, y_min, y_max = min(x_min), max(x_max), min(y_min), max(y_max)
     dx = (x_max - x_min)*0.05
     dy = (y_max - y_min)*0.05
 
@@ -32,6 +37,8 @@ def animate(list_paths, list_vectors=None, anim_time=4, repeat=True, scale=1, ve
     lines = [ax.plot([], [], lw=2, color='tab:blue')[0] for _ in range(len(M))]
     
     if list_vectors is not None:
+        if isinstance(list_vectors[0], np.ndarray):
+            list_vectors = [list_vectors]
         PAs, vecs = [], []
         for PA, vec in list_vectors:
             PAs.append(PA)
@@ -52,7 +59,7 @@ def animate(list_paths, list_vectors=None, anim_time=4, repeat=True, scale=1, ve
         lines.append(ax.quiver([1000]*n,[1000]*n, [0]*n, [0]*n, scale=1/vector_scale, scale_units='xy', cmap='plasma'))
         def _animate_(i):
             for lnum, line in enumerate(lines[:-1]):
-                line.set_data(M[lnum, i, 0], M[lnum, i, 1])
+                line.set_data(M[lnum][i, 0], M[lnum][i, 1])
             lines[-1].set_UVC(vecs[i, 0], vecs[i, 1])
             lines[-1].set_offsets(PAs[i].T)
             lines[-1].set_facecolor(color_array[i])
@@ -60,11 +67,10 @@ def animate(list_paths, list_vectors=None, anim_time=4, repeat=True, scale=1, ve
     else:
         def _animate_(i):
             for lnum, line in enumerate(lines):
-                line.set_data(M[lnum, i, 0], M[lnum, i, 1])
+                line.set_data(M[lnum][i, 0], M[lnum][i, 1])
             return lines
     
-    n = M.shape[1]
-    anim = animation.FuncAnimation(fig, _animate_, frames=n, interval=anim_time*1000/n, blit=True, repeat=repeat)    
+    anim = animation.FuncAnimation(fig, _animate_, frames=(n:=len(M[0])), interval=anim_time*1000/n, blit=True, repeat=repeat)    
     
     return anim
 
@@ -77,17 +83,15 @@ def make_continuous(L):
             l0 = L[i]
     return L
 
-
-distance = lambda point1, point2: np.sqrt(np.sum((point2 - point1)**2, axis=0))
-
+distance = lambda p1, p2: np.sqrt(np.sum((p2 - p1)**2, axis=0))
 
 def get_speed(L, t):
     if len(L.shape) == 1:    
-        return (L[1:] - L[:-1])/(t/(len(L) - 1))
-    return (L[:, 1:] - L[:, :-1])/(t/(L.shape[1] - 1))
+        return np.diff(L)/t*(len(L) - 1)
+    return np.diff(L, axis=1)/t*(L.shape[1] - 1)
 
 
-def get_speed_precis(L, t):
+def get_speed_accurate(L, t):
     if len(L.shape) == 1:    
         return (L[2:] - L[:-2])/(2*t/(len(L) - 1))
     return (L[:, 2:] - L[:, :-2])/(2*t/(L.shape[1] - 1))
@@ -95,8 +99,5 @@ def get_speed_precis(L, t):
 
 def get_acceleration(L, t):
     if len(L.shape) == 1:    
-        return (L[2:] - 2*L[1:-1] + L[:-2])/(t/(len(L) - 1))**2
-    return (L[:, 2:] - 2*L[:, 1:-1] + L[:, :-2])/(t/(L.shape[1] - 1))**2
-
-# prendre en compte chemins de diff taille
-# prolongement de vitesse et d'acc pour résoudre le pb de shape, fct np.diff
+        return np.diff(L, 2)/(t/(len(L) - 1))**2
+    return np.diff(L, 2, axis=1)/(t/(L.shape[1] - 1))**2
