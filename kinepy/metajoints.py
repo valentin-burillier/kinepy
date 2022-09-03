7e338a909684efb6eed2ea417244d007ae8aa9ad
EKnight-Blue <loicchechevalier@gmail.com>
2022-09-03 14:13:36 +0200

    Correction rel_block

--- kinepy/metajoints.py
+++ kinepy/metajoints.py
@@ -28,28 +28,33 @@ class Gear(LinearRelation):
         self.pressure_angle = pa
 
     def rel_block(self, j_index, eq1s1, eq2s2):
-        j, j_ = (self.system.joints[self.j1], self.system.joints[self.j2])[:: 2 * j_index - 1]
-        a, b = j.point, j_.point
-        d = mag(b - a)
-        c1 = j.s1 == j_.s1 or j.s2 == j_.s2
-        r = (2 * c1 - 1) * self.r ** (-2 * j_index + 1)
-        rad = r * d / (r - 1)
-        theta = angle2(b - a, 1/d)
-
         (_, s1), (eq2, s2) = eq1s1, eq2s2
-        m_12 = tmd(self.system, a, eq2)
-        normal_12 = m_12 / rad
-        tangent_12 = np.tan(self.pressure_angle) * np.abs(normal_12) * (2 * (rad < 0) - 1) * (2 * ((j_.s1, j_.s2)[c1 ^ (s2 == j.s1)] == s2) - 1)
-        f_12 = mat_mul_n(rot(theta), (tangent_12, normal_12))
-        f_s1_s2 = trd(self.system, eq2) - f_12
+        j1, j2 = (self.system.joints[self.j1], self.system.joints[self.j2])[::(1, -1)[j_index]]
 
-        self.system.sols[s1].mech_actions.append(MechanicalAction(-f_s1_s2, a, 0.))
-        self.system.sols[s2].mech_actions.append(MechanicalAction(f_s1_s2, a, 0.))
+        # indices du solide commun
+        i1, i2 = (j1.s2 == j2.s1) or (j1.s2 == j2.s2),  (j1.s1 == j2.s2) or (j1.s2 == j2.s2)
+        r = (self.r * (2 * (i1 ^ i2) - 1)) ** (1, -1)[j_index]
+
+        # s2 est-il le solide en commun
+        c = s2 == (j1.s1, j1.s2)[i1]
+
+        a, b = j1.point, j2.point
+        d = mag(b - a)
+        theta = angle2(b - a, 1 / d)
+        rad = r * d / (r - 1)
+
+        m_12 = tmd(self.system, a, eq2)
+        n_12 = m_12 / rad
+        t_12 = np.abs(n_12) * np.tan(self.pressure_angle) * (2 * ((rad < 0) ^ c) - 1)
+        f_12 = mat_mul_n(rot(theta), (t_12, n_12))
+        f_s12 = trd(self.system, eq2) - f_12
+
+        self.system.sols[s1].mech_actions.append(MechanicalAction(-f_s12, a, 0.))
+        self.system.sols[s2].mech_actions.append(MechanicalAction(f_s12, a, 0.))
 
         p = a + rad * unit(theta)
-        s_1, s_2 = (((j_.s1, j_.s2)[c1 ^ (s2 == j.s1)], s2), (s1, (j_.s1, j_.s2)[c1 ^ (s1 == j.s1)]))[(j_.s1, j_.s2)[c1 ^ (s2 == j.s1)] == s2]
-        self.system.sols[s_1].mech_actions.append(MechanicalAction(-f_12, p, 0.))
-        self.system.sols[s_2].mech_actions.append(MechanicalAction(f_12, p, 0.))
+        self.system.sols[(s1, (j2.s1, j2.s2)[not i2])[not c]].mech_actions.append(MechanicalAction(-f_12, p, 0.))
+        self.system.sols[((j2.s1, j2.s2)[not i2], s2)[not c]].mech_actions.append(MechanicalAction(f_12, p, 0.))
 
 
 class GearRack(LinearRelation):
