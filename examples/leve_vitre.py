78bf8c21c02dea549654b704ca2fd95a1af410d8
Burillier Valentin <93446869+valentin-burillier@users.noreply.github.com>
2023-04-18 17:40:26 +0200

    Update leve_vitre.py

--- examples/leve_vitre.py
+++ examples/leve_vitre.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 
 #%%
 
+set_unit(SPEED, 0.001, unit='mm/s')
 show_units()
 
 #%%
 
-sys = k.System()
-
 rA, rB, l = 7, 70, 130
 
+sys = k.System()
 
 s1 = sys.add_solid('Gear wheel', m=0.2)
 s2 = sys.add_solid('Crank', m=0.5, g=(l, 0))
@@ -30,6 +30,8 @@ ps2 = sys.add_pin_slot(4, 2, p2=(2*l, 0))
 
 sys.pilot(r1)
 
+sys.add_gravity()
+
 sys.compile()
 
 sys.change_signs({'3 RRP':-1, '4 RRP':-1})
@@ -38,17 +40,22 @@ sys.change_signs({'3 RRP':-1, '4 RRP':-1})
 
 t, n = 3, 101
 time = np.linspace(0, t, n)
-angle = to.trapezoidal_input(-np.pi/4*rB/rA, np.pi/4*rB/rA, t, n, v_max=6.)
+angle = to.trapezoidal_angular_input(-np.pi/4*rB/rA, np.pi/4*rB/rA, t, n, v_max=6)
 sys.solve_dynamics(angle, t)
 
-#%%
+#%%a
 
 P = s1.get_point((-rA, 0))
 G = s2.get_point((-rB, 0))
 to.animate([[G, ps2.point, r4.point, ps1.point], [P, s1.origin]])
-plt.show()
+
 #%%
 
 plt.plot(time, s4.origin[1])
 plt.plot(time, ps1.sliding)
-plt.show()
+
+#%%
+
+plt.plot(ps1.normal)
+plt.plot(to.norm(r1.force))
+
