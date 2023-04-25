e53f591bf510e820a795665ca9077e1281f4ba2d
EKnight-Blue <loicchechevalier@gmail.com>
2023-04-25 19:42:49 +0200

    lisibilité

--- kinepy/gui/__init__.py
+++ kinepy/gui/__init__.py
@@ -168,10 +168,6 @@ from kinepy.math.geometry import unit, z_cross
 
 FPS = 60
 PATH = os.path.dirname(__file__)
-SOLID_COLORS = (
-    (225, 105, 97), (255, 180, 128), (248, 243, 141), (66, 214, 164), (8, 202, 209), (89, 173, 246), (157, 148, 255),
-    (199, 128, 232)
-)
 
 
 class GUI(Camera):
@@ -206,92 +202,21 @@ class GUI(Camera):
                 self.running = False
             Camera.manage(self, event)
 
-    def draw_rev(self, rev, data):
-        pg.draw.circle(
-            self.surface,
-            self.background,
-            self.real_to_screen(data[:, int(self.animation_state)]),
-            REVOLUTE_RADIUS * self.scale / self.scale0 * self.zoom
-        )
-        pg.draw.circle(
-            self.surface,
-            SOLID_COLORS[rev.s2.rep % len(SOLID_COLORS)],
-            self.real_to_screen(data[:, int(self.animation_state)]),
-            REVOLUTE_RADIUS * self.scale / self.scale0 * self.zoom, 2
-        )
-
-    def draw_pri(self, pri, data):
-        p1, p2, p3 = data
-        frame = int(self.animation_state)
-        pg.draw.line(
-            self.surface,
-            SOLID_COLORS[pri.s1.rep % len(SOLID_COLORS)],
-            self.real_to_screen(p1[:, frame]),
-            self.real_to_screen(p2[:, frame]),
-            2
-        )
-        rec = self.real_to_screen(p3[:, frame]) + np.einsum('ik,lk->li', rot(pri.s2.angle[frame] + pri.a2), PRISMATIC * self.scale / self.scale0 * self.zoom)
-        pg.draw.polygon(
-            self.surface,
-            self.background,
-            rec,
-            0
-        )
-        pg.draw.polygon(
-            self.surface,
-            SOLID_COLORS[pri.s2.rep % len(SOLID_COLORS)],
-            rec,
-            2
-        )
-
-    def draw_pin(self, pin, data):
-        p1, p2, p3 = data
-        frame = int(self.animation_state)
-        pg.draw.line(
-            self.surface,
-            SOLID_COLORS[pin.s1.rep % len(SOLID_COLORS)],
-            self.real_to_screen(p1[:, frame]),
-            self.real_to_screen(p2[:, frame]),
-            2
-        )
-        rec = self.real_to_screen(p3[:, frame]) + np.einsum('ik,lk->li', rot(pin.s2.angle[frame]), PIN_SLOT * self.scale / self.scale0 * self.zoom)
-        pg.draw.polygon(
-            self.surface,
-            self.background,
-            rec,
-            0
-        )
-        pg.draw.polygon(
-            self.surface,
-            SOLID_COLORS[pin.s2.rep % len(SOLID_COLORS)],
-            rec,
-            2
-        )
-
-    draw_joint = {
-        1: 'draw_rev',
-        2: 'draw_pri',
-        3: 'draw_pin'
-    }
-
     def draw(self):
         self.surface.fill(self.background)
-        animation_index = int(self.animation_state)
-        # for index, point in enumerate(self.points):
-        #     pg.draw.circle(self.surface, SOLID_COLORS[index % len(SOLID_COLORS)], self.real_to_screen(point[:, animation_index]), max(1, 5 * self.scale / self.scale0 * self.zoom), 2)
+        frame = int(self.animation_state)
 
         # drawing references
         for index, sol in enumerate(self.system.sols):
-            origin = self.real_to_screen(sol.origin[:, animation_index])
-            u = unit(-sol.angle[animation_index]) * 40 * self.scale / self.scale0 * self.zoom
+            origin = self.real_to_screen(sol.origin[:, frame])
+            u = unit(-sol.angle[frame]) * 40 * self.scale / self.scale0 * self.zoom
             draw_arrow2(self.surface, SOLID_COLORS[index % len(SOLID_COLORS)], origin, origin + u)
             draw_arrow2(self.surface, SOLID_COLORS[index % len(SOLID_COLORS)], origin, origin - z_cross(u))
 
         for j, data in zip(self.system.joints, self.prepared_joints):
-            if j.id_ not in self.draw_joint:
+            if j.id_ not in draw_joint:
                 continue
-            getattr(self, self.draw_joint[j.id_])(j, data)
-
+            draw_joint[j.id_](self, j, data)
 
     def tick(self):
         self.animation_state = (self.animation_state + self.animation_speed) % self.system.n
--- kinepy/gui/drawing_tool.py
+++ kinepy/gui/drawing_tool.py
@@ -70,3 +70,89 @@ prepare = {
 
 def prepare_joints(joints):
     return [prepare[j.id_](j) if j.id_ in prepare else () for j in joints]
+
+
+SOLID_COLORS = (
+    (225, 105, 97), (255, 180, 128), (248, 243, 141), (66, 214, 164), (8, 202, 209), (89, 173, 246), (157, 148, 255),
+    (199, 128, 232)
+)
+
+
+def draw_rev(self, rev, data):
+    pg.draw.circle(
+        self.surface,
+        self.background,
+        self.real_to_screen(data[:, int(self.animation_state)]),
+        REVOLUTE_RADIUS * self.scale / self.scale0 * self.zoom
+    )
+    pg.draw.circle(
+        self.surface,
+        SOLID_COLORS[rev.s2.rep % len(SOLID_COLORS)],
+        self.real_to_screen(data[:, int(self.animation_state)]),
+        REVOLUTE_RADIUS * self.scale / self.scale0 * self.zoom, 2
+    )
+
+
+def draw_pri(self, pri, data):
+    p1, p2, p3 = data
+    frame = int(self.animation_state)
+    pg.draw.line(
+        self.surface,
+        SOLID_COLORS[pri.s1.rep % len(SOLID_COLORS)],
+        self.real_to_screen(p1[:, frame]),
+        self.real_to_screen(p2[:, frame]),
+        2
+    )
+    rec = self.real_to_screen(p3[:, frame]) + np.einsum(
+        'ik,lk->li',
+        rot(pri.s2.angle[frame] + pri.a2),
+        PRISMATIC * self.scale / self.scale0 * self.zoom
+    )
+    pg.draw.polygon(
+        self.surface,
+        self.background,
+        rec,
+        0
+    )
+    pg.draw.polygon(
+        self.surface,
+        SOLID_COLORS[pri.s2.rep % len(SOLID_COLORS)],
+        rec,
+        2
+    )
+
+
+def draw_pin(self, pin, data):
+    p1, p2, p3 = data
+    frame = int(self.animation_state)
+    pg.draw.line(
+        self.surface,
+        SOLID_COLORS[pin.s1.rep % len(SOLID_COLORS)],
+        self.real_to_screen(p1[:, frame]),
+        self.real_to_screen(p2[:, frame]),
+        2
+    )
+    rec = self.real_to_screen(p3[:, frame]) + np.einsum(
+        'ik,lk->li',
+        rot(pin.s2.angle[frame]),
+        PIN_SLOT * self.scale / self.scale0 * self.zoom
+    )
+    pg.draw.polygon(
+        self.surface,
+        self.background,
+        rec,
+        0
+    )
+    pg.draw.polygon(
+        self.surface,
+        SOLID_COLORS[pin.s2.rep % len(SOLID_COLORS)],
+        rec,
+        2
+    )
+
+
+draw_joint = {
+    1: draw_rev,
+    2: draw_pri,
+    3: draw_pin
+}
