05c8fb595690decde649d585796d7762111418cc
EKnight-Blue <loicchechevalier@gmail.com>
2023-04-28 18:40:47 +0200

    Belle Grille avec bonnes unités, ancien travail viré

--- kinepy/gui/grid_manager.py
+++ kinepy/gui/grid_manager.py
@@ -1,4 +1,5 @@
 from kinepy.gui.camera import Camera
+import kinepy.units as units
 import numpy as np
 import pygame as pg
 
@@ -13,24 +14,24 @@ class GridManager(Camera):
     borders = 0, 0, 0, 0
 
     def find_unit(self):
-        log = np.log10(GRID_CELL / self.zoom / self.scale)
-        self.unit = 10 ** (min(SCALES, key=lambda x: (x - (log % 1)) ** 2) + np.floor(log))
-        print(f'\r{self.unit}', end='')
+        log = np.log10(GRID_CELL / self.zoom / self.scale / units.SYSTEM[units.LENGTH])
+        self.unit = 10 ** (min(SCALES, key=lambda x: abs(x - (log % 1))) + np.floor(log))
+        print(f'\r{self.unit:.1e} {units.get_unit(units.LENGTH)}', end='')
 
     def camera_borders(self):
-        start_x = self.camera_area.x // self.unit
-        start_y = self.camera_area.y // self.unit
-        end_x = (self.camera_area.x + self.camera_area.w) // self.unit + 1
-        end_y = (self.camera_area.y + self.camera_area.h) // self.unit + 1
+        start_x = self.camera_area.x / units.SYSTEM[units.LENGTH] // self.unit
+        start_y = self.camera_area.y / units.SYSTEM[units.LENGTH] // self.unit
+        end_x = (self.camera_area.x + self.camera_area.w) / units.SYSTEM[units.LENGTH] // self.unit + 1
+        end_y = (self.camera_area.y + self.camera_area.h) / units.SYSTEM[units.LENGTH] // self.unit + 1
         self.borders = tuple(int(x) for x in (start_x, start_y, end_x, end_y))
 
     def draw_grid(self, color):
         camera_center = self.camera_area.center
         for x in range(self.borders[0], self.borders[2]):
-            line_x = (x * self.unit - camera_center[0]) * self.scale * self.zoom + self.surface.get_rect().centerx
+            line_x = (x * self.unit * units.SYSTEM[units.LENGTH] - camera_center[0]) * self.scale * self.zoom  + self.surface.get_rect().centerx
             pg.draw.line(self.surface, color, (line_x, 0), (line_x, self.surface.get_height()), 1)
         for y in range(self.borders[1], self.borders[3]):
-            line_y = -(y * self.unit - camera_center[1]) * self.scale * self.zoom + self.surface.get_rect().centery
+            line_y = -(y * self.unit * units.SYSTEM[units.LENGTH] - camera_center[1]) * self.scale * self.zoom + self.surface.get_rect().centery
             pg.draw.line(self.surface, color, (0, line_y), (self.surface.get_width(), line_y), 1)
 
     def change_scale(self, event):
