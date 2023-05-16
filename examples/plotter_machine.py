de6168eb62e9dc7906aec18ee67e464c0a7c47a5
Burillier Valentin <93446869+valentin-burillier@users.noreply.github.com>
2023-05-16 17:01:54 +0200

    plotter propre

--- examples/plotter_machine.py
+++ examples/plotter_machine.py
@@ -1,6 +1,7 @@
 import kinepy as k
 from kinepy.units import *
 import kinepy.tools as to
+import kinepy.gui as kd
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,17 +18,17 @@ sys = k.System()
 l = 1000
 
 s1 = sys.add_solid('Tige gauche')
-s2 = sys.add_solid('Corps gauche')
+s2 = sys.add_solid('Corps gauche', m=1.)
 s3 = sys.add_solid('Corps droit')
 s4 = sys.add_solid('Tige droite')
 s5 = sys.add_solid('Guide vertical')
 s6 = sys.add_solid('Guide horizontal')
 
 r1 = sys.add_revolute(0, 1)
-r2 = sys.add_revolute(0, 4)
+r2 = sys.add_revolute(0, 4, p1=(l, 0))
 r3 = sys.add_revolute(2, 3)
 p1 = sys.add_prismatic(1, 2)
-p2 = sys.add_prismatic(3, 4)
+p2 = sys.add_prismatic(4, 3)
 
 r4 = sys.add_revolute(5, 2)
 p3 = sys.add_prismatic(0, 6)
@@ -35,28 +36,36 @@ p4 = sys.add_prismatic(6, 5, a1=np.pi/2, a2=np.pi/2)
 
 sys.pilot(p3, p4)
 
+sys.block(p1, p2)
+
+sys.add_gravity()
+
 sys.compile()
 
 #%%
 
-t, n = 10, 1001
+t, n = 5, 1001
+time = np.linspace(0, t, n)
 
-
-r = 100
+r = 350
 x0, y0 = 600, -500
-angle = np.linspace(0, 2*np.pi, n)
+angle = to.sinusoidal_input(0, 2*np.pi, t, n, v_max=1.9)
 
 x, y = x0 - r*np.cos(angle), y0 + r*np.sin(angle)
 
-plt.axis('equal')
-plt.plot(x[:900], y[:900])
+#%%
 
+sys.solve_dynamics([x, y], t)
 
 #%%
 
+kd.system(sys)
 
-y0 = -400
-x, y = np.linspace(0, l, n), np.full((n,), y0)
+kd.animation_time(t)
+kd.grid()
+kd.graduation()
+kd.light_mode()
+kd.dark_mode()
+kd.frames_of_reference()
 
-plt.axis('equal')
-plt.plot(x[:900], y[:900])
+kd.show()
