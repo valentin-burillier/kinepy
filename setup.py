9978518644ef8569b49c433051e83a904ad9186b
Burillier Valentin <93446869+valentin-burillier@users.noreply.github.com>
2023-05-17 08:59:13 +0200

    nouveau upload

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name='kinepy',
     packages=['kinepy'],
-    version='0.0.3',
+    version='0.1.0',
     description='Python library dedicated to plane mechanism simulation',
     author='Loïc Chevalier - Valentin Burillier',
     url='https://github.com/valentin-burillier/kinepy',
-    keywords=['robotics', 'dynamic', 'kinematics', 'static', 'inverse-kinematics', 'mechanism'],
+    keywords=['robotics', 'simulation', 'dynamics', 'kinematics', 'statics', 'inverse-kinematics', 'mechanism'],
     requires=['numpy', 'matplotlib'],
 )
