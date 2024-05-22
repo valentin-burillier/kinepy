537834f225f2f87a2a30ee69d06c8e4ff3cbcd1e
Burillier Valentin <93446869+valentin-burillier@users.noreply.github.com>
2024-05-22 20:32:27 +0200

    polynomial_input added

--- kinepy/tools.py
+++ kinepy/tools.py
@@ -1,156 +1,105 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import animation
-from matplotlib import cm
-import matplotlib.colors as mcol
 
 from kinepy.units import *
 from kinepy.interface.decorators import physics_input_function, physics_output, physics_input_function_variable
 
-# ---------------------------------------------------- Animate -------------------------------------------------------------
-
-global C
-# C = cm.get_cmap('coolwarm') # plasma, coolwarm, bwr, rainbow, OrRd
-C = mcol.LinearSegmentedColormap.from_list("", ["cyan", "r"])
-
-
-def animate(list_paths, list_vectors=None, anim_time=4, repeat=True, scale=1, vector_scale=0.1, magnitude_vector='proportionnal'):
-    """
-    magnitude_vector : 'proportionnal', 'log', 'unitary'
-    """
-    if isinstance(list_paths[0], np.ndarray):
-        list_paths = [list_paths]
-        
-    M = []
-    x_min, x_max, y_min, y_max = [], [], [], []
-    for path in list_paths:
-        A = np.array(path)
-        nan = np.any(np.isnan(A), axis=(1, 0))
-        A[:, :, nan] = 0
-        M.append(A.transpose((2, 1, 0)))
-        x_min.append(A[:, 0].min())
-        x_max.append(A[:, 0].max())
-        y_min.append(A[:, 1].min())
-        y_max.append(A[:, 1].max())
-
-    x_min, x_max, y_min, y_max = min(x_min), max(x_max), min(y_min), max(y_max)
-    dx = (x_max - x_min)*0.05
-    dy = (y_max - y_min)*0.05
-
-    fig = plt.figure(figsize=((x_max - x_min)/(y_max - y_min)*scale*7, scale*7))
-    ax = plt.axes(xlim=(x_min - dx, x_max + dx), ylim=(y_min - dy, y_max + dy))
-    lines = [ax.plot([], [], lw=2, color='tab:blue')[0] for _ in range(len(M))]
-    
-    if list_vectors is not None:
-        if isinstance(list_vectors[0], np.ndarray):
-            list_vectors = [list_vectors]
-        PAs, vecs = [], []
-        for PA_, vec_ in list_vectors:
-            PA, vec = PA_.copy(), vec_.copy()
-            nan = np.any(np.isnan(PA), axis=0)
-            PA[:, nan] = 0
-            PAs.append(PA)
-            nan = np.any(np.isnan(vec), axis=0)
-            vec[:, nan] = 0
-            vecs.append(vec)
-        PAs = np.transpose(PAs, (2 ,1, 0))
-        vecs = np.transpose(vecs, (2 ,1, 0))
-        magnitudes = np.sqrt(np.sum(vecs**2, axis=1)) # linalg
-        colors = magnitudes/np.max(magnitudes)
-        color_array = np.array([C(color) for color in colors])
-        
-        if magnitude_vector == 'log':
-            a = np.log(magnitudes + 1)/magnitudes
-            vecs *= a[:, np.newaxis, :]
-        elif magnitude_vector == 'unitary':
-            vecs /= magnitudes[:, np.newaxis, :]
-        
-        n = PAs.shape[2]
-        lines.append(ax.quiver([10**10]*n,[10**10]*n, [0]*n, [0]*n, scale=1/vector_scale, scale_units='xy', cmap='plasma'))
-
-        def _animate_(i):
-            for lnum, line in enumerate(lines[:-1]):
-                line.set_data(M[lnum][i, 0], M[lnum][i, 1])
-            lines[-1].set_UVC(vecs[i, 0], vecs[i, 1])
-            lines[-1].set_offsets(PAs[i].T)
-            lines[-1].set_facecolor(color_array[i])
-            return lines
-    else:
-        def _animate_(i):
-            for lnum, line in enumerate(lines):
-                line.set_data(M[lnum][i, 0], M[lnum][i, 1])
-            return lines
-    
-    anim = animation.FuncAnimation(fig, _animate_, frames=(n:=len(M[0])), interval=anim_time*1000/n, blit=True, repeat=repeat)    
-    
-    return anim
 # ---------------------------------------------------- Inputs ----------------------------------------------------------
 
+def __sgn(ordre):
+    suite = np.array([1])
+    for i in range(ordre - 1):
+        suite = np.r_[suite, -suite]
+    return suite
 
-@physics_input_function_variable(VARIABLE_UNIT, VARIABLE_UNIT, TIME, '', VARIABLE_DERIVATIVE)
-def direct_input(a, b, t, n=101, v_max=None):
-    if v_max is not None and abs((b - a) / t) > v_max:
-        raise ValueError("Speed is too high")
-    return np.linspace(a, b, n)
+def __suite_t(ordre, t):
+    suite = np.array([0])
+    for i in range(ordre - 1):
+        suite += 1
+        if t[ordre - 2 - i] == 0:
+            suite = np.r_[suite, suite]
+        else:
+            suite = np.r_[suite, 0, suite]
+    return suite
 
+__calcul = lambda ordre, p, x : np.sum(p*x**np.arange(0, ordre + 1))
 
-@physics_input_function_variable(VARIABLE_UNIT, VARIABLE_UNIT, TIME, '', VARIABLE_DERIVATIVE, VARIABLE_SECOND_DERIVATIVE)
-def trapezoidal_input(a, b, t, n=101, v_max=None, a_max=None):
-    time_line = np.linspace(0, t, n)
-    v = 2 * (b - a) / t
-
-    # triangle case
-    if v_max is None or abs(v) <= v_max:
-        acc = 4 * (b - a) / (t * t)
-        if a_max is not None and abs(acc) > a_max:
-            raise ValueError("Too much Acceleration")
-        t0, t1 = time_line[:n//2], time_line[n//2:]
-        return np.r_[a + .5 * acc * t0 * t0, b - .5 * acc * (t1 - t) ** 2]
-
-    # trapezoid case
-    if 2 * v_max < abs(v):
-        raise ValueError("Impossible input")
-    v = np.sign(v) * v_max
-    acc = v_max / (t_acc := t - (b - a) / v)
-    if a_max is not None and abs(acc) > a_max:
-        raise ValueError("Too much acceleration")
-    cutting_index = int(n * t_acc / t)
-    if cutting_index:
-        t0, t1, t2 = time_line[:cutting_index], time_line[cutting_index:-cutting_index], time_line[-cutting_index:]
-    else:
-        t0, t1, t2 = np.array([], float), time_line, np.array([], float)
-    x0 = a + .5 * acc * t_acc * t_acc
-    return np.r_[a + .5 * acc * t0 * t0, x0 + v * (t1 - t_acc), b - .5 * acc * (t2 - t) ** 2]
+def __deriv(ordre, p):
+    p[:ordre] = p[1:]*np.arange(1, ordre + 1)
+    p[ordre] = 0
+    return p
 
 
-@physics_input_function_variable(VARIABLE_UNIT, VARIABLE_UNIT, TIME, '', VARIABLE_DERIVATIVE, VARIABLE_SECOND_DERIVATIVE)
-def sinusoidal_input(a, b, t, n=101, v_max=None, a_max=None):
-    time_line = np.linspace(0, t, n)
-    v = 2 * (b - a) / t
+def __creation_tab(ordre, suite, t):
+    tab = np.zeros((len(suite), ordre + 1))
+    tab[suite==ordre-1, ordre] = const[ordre - 1]/np.math.factorial(ordre)*__sgn(ordre)
+    tab[0, 0] = a
+    for i in range(len(suite) - 1):
+        dt = t[suite[i]]
+        p = tab[i].copy()
+        tab[i+1, 0] = __calcul(ordre, p, dt)
+        for d in range(1, ordre):
+            p = __deriv(ordre, p)
+            tab[i+1, d] = __calcul(ordre, p, dt)/np.math.factorial(d)
+    return tab
+    
+def __trouver_const(a, b, const, ordre, iteration):
+    c = [b - a] + const
+    k = [c[i]/c[i + 1] for i in range(ordre)]
+    
+    hist = []
+    for _ in range(iteration):
+        for i in range(ordre - 1):
+            sum_k = sum(k[i+1:])
+            if i in hist or k[i] < sum_k:
+                if i not in hist:
+                    hist.append(i)
+                if i + 2 > ordre:
+                    c[i+1] = np.sqrt(c[i]*c[i+2])
+                else:
+                    b = sum_k - k[i+1]
+                    det = b**2 + 4*c[i]/c[i+2]
+                    c[i+1] = (-b + np.sqrt(det))/2*c[i+2]
+                k[i] = c[i]/c[i+1]
+                k[i+1] = c[i+1]/c[i+2]
+                
+    return c, k
 
-    # "triangle" case
-    if v_max is None or abs(v) <= v_max:
-        acc = np.pi * v / t
-        if a_max is not None and abs(acc) > a_max:
-            raise ValueError("Too much Acceleration")
-        omega = 2 * np.pi / t
-        return a + .5 * v * (time_line - np.sin(time_line * omega) / omega)
+def polynomial_input(a, b, const, n=101, iteration=100):
+    ordre = len(const)
 
-    # "trapezoid" case
-    if 2 * v_max < abs(v):
-        raise ValueError("Impossible input")
-    v = np.sign(v) * v_max
-    acc = .5 * np.pi * v_max / (t_acc := t - (b - a) / v)
-    if a_max is not None and abs(acc) > a_max:
-        raise ValueError("Too much acceleration")
+    c, k = __trouver_const(a, b, const, ordre, iteration)
 
-    cutting_index = int(n * t_acc / t)
-    if cutting_index:
-        t0, t1, t2 = time_line[:cutting_index], time_line[cutting_index:-cutting_index], time_line[-cutting_index:]
-    else:
-        t0, t1, t2 = np.array([], float), time_line, np.array([], float)
-    omega, x0 = np.pi / t_acc, a + .5 * v * t_acc
-    return np.r_[a + .5 * v * (t0 - np.sin(omega * t0) / omega), x0 + v * (t1 - t_acc), b + .5 * v * (t2 - t - np.sin(omega * (t2 - t)) / omega)]
+    t = []
+    sum_k = sum(k)
+    for i in range(ordre):
+        sum_k -= k[i]
+        t_i = k[i] - sum_k
+        t.append(t_i if t_i > 10e-15 else 0)
+    
+    suite = __suite_t(ordre, t)
+    tab = __creation_tab(ordre, suite, t)
+    X = np.linspace(0, np.sum(t*2**np.arange(0, ordre)), n)
+    Y = []
+    i = 0
+    x_inf = 0
+    x_sup = t[suite[0]]
+    for x in X:
+        while x > x_sup and i + 1 < len(suite):
+            i += 1
+            x_inf = x_sup
+            x_sup += t[suite[i]]
+        Y.append(__calcul(ordre, tab[i], x - x_inf))
+    return X, np.array(Y)
+
+
+@physics_input_function_variable(VARIABLE_UNIT, VARIABLE_UNIT, VARIABLE_DERIVATIVE, '', '')
+def direct_input(a, b, v_max, n=101, iteration=100):
+    polynomial_input(a, b, [v_max], n=n, iteration=iteration)
+
+@physics_input_function_variable(VARIABLE_UNIT, VARIABLE_UNIT, VARIABLE_DERIVATIVE, VARIABLE_SECOND_DERIVATIVE, '', '')
+def trapezoidal_input(a, b, v_max, a_max, n=101, iteration=100):
+    polynomial_input(a, b, [v_max, a_max], n=n, iteration=iteration)
 
 
 # ---------------------------------------------------- Geometry --------------------------------------------------------
