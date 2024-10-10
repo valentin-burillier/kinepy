3d6e168c53448772a690ec96568a4b1f60f065c8
Loic Chevalier <loicchechevalier@gmail.com>
2024-10-11 01:27:57 +0200

    Fixed wierd indentation

--- tests/test.cpp
+++ tests/test.cpp
@@ -51,18 +51,18 @@ TEST(FindIsomorphism, Identity) {
 TEST(FindIsomorphism, Shuffled) {
     std::vector<GraphNode> user_graph(adjacency_size(3));
     std::vector<JointDegree> user_degree(3);
-        for (auto const &isomorphism: std::vector<std::vector<uint32_t>>{{{0, 1, 2}, {0, 2, 1}, {1, 0, 2}, {1, 2, 0}, {2, 0, 1}, {2, 1, 0}}}) {
-            for (int g = 0; g < ISOSTATIC_GRAPH_COUNT && ISOSTATIC_GRAPHS[g].vertex_count == 3; ++g) {
-                IsostaticGraphInfo const &target = ISOSTATIC_GRAPHS[g];
-                isostatic_isomorphism(&target, isomorphism, user_graph, user_degree);
+    for (auto const &isomorphism: std::vector<std::vector<uint32_t>>{{{0, 1, 2}, {0, 2, 1}, {1, 0, 2}, {1, 2, 0}, {2, 0, 1}, {2, 1, 0}}}) {
+        for (int g = 0; g < ISOSTATIC_GRAPH_COUNT && ISOSTATIC_GRAPHS[g].vertex_count == 3; ++g) {
+            IsostaticGraphInfo const &target = ISOSTATIC_GRAPHS[g];
+            isostatic_isomorphism(&target, isomorphism, user_graph, user_degree);
 
-                uint32_t result_graph;
-                uint32_t *result_isomorphism;
-                result_graph = find_isomorphism(user_graph.data(), user_degree.data(), target.vertex_count, &result_isomorphism);
-                std::vector<uint32_t> ri;
+            uint32_t result_graph;
+            uint32_t *result_isomorphism;
+            result_graph = find_isomorphism(user_graph.data(), user_degree.data(), target.vertex_count, &result_isomorphism);
+            std::vector<uint32_t> ri;
 
-                EXPECT_EQ(result_graph, g);
-                free(result_isomorphism);
-            }
+            EXPECT_EQ(result_graph, g);
+            free(result_isomorphism);
+        }
     }
 }
