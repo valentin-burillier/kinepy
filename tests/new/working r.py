88ce20de7094176443fec0a8a4c90d91b5a28a0d
Burillier Valentin <93446869+valentin-burillier@users.noreply.github.com>
2025-04-07 19:07:59 +0200

    Create working r.py

---
+++ tests/new/working r.py
@@ -0,0 +1,35 @@
+import kinepy as kp
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%%
+
+sys = kp.System()
+
+s0 = sys.ground
+s1 = sys.add_solid(mass=3, g=(0, 2))
+
+r1 = sys.add_revolute(s0, s1)
+
+r1.pilot()
+r1.work()
+
+sys.determine_computation_order()
+n = 101
+sys.set_frame_count(n)
+
+sys.add_interaction(kp.Gravity(g=(0, -1)))
+
+#%%
+
+angle = np.linspace(0, 2*np.pi, n)
+r1.set_input(angle)
+
+sys.solve_kinematics()
+
+#%%
+
+sys.solve_dynamics()
+
+plt.plot(r1.get_value(), r1.get_torque())
+plt.show()
