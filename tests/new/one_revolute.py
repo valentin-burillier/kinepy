78b03acc2cccc036cd6f42a7cc4b30a501d1294b
Burillier Valentin <93446869+valentin-burillier@users.noreply.github.com>
2025-05-05 18:06:31 +0200

    Update one_revolute.py

--- tests/new/one_revolute.py
+++ tests/new/one_revolute.py
@@ -7,14 +7,15 @@ import matplotlib.pyplot as plt
 sys = kp.System()
 
 s0 = sys.ground
-s1 = sys.add_solid(g=(1, 0), mass=2)
+s1 = sys.add_solid(g=(0, 0), mass=2, moment_of_inertia=3)
 
 r1 = sys.add_revolute(s0, s1)
 
 r1.pilot()
 r1.work()
 
-sys.add_gravity(g=(0, -10))
+#sys.add_gravity(g=(0, -10))
+sys.add_inertia()
 
 sys.determine_computation_order()
 n = 101
@@ -22,7 +23,7 @@ sys.set_sim_parameters(n, 5)
 
 #%%
 
-angle = np.linspace(0, 2*np.pi, n)
+angle = 2*np.pi*np.linspace(0, 1, n)**2
 r1.set_input(angle)
 
 sys.solve_kinematics()
@@ -31,10 +32,9 @@ sys.solve_kinematics()
 
 kd = sys.kinematic_diagram()
 
-#kd.add_solid_point(s1, (1, 0))
+kd.add_solid_point(s1, (1, 0))
 
 kd.show()
-
 
 #%%
 
@@ -43,3 +43,5 @@ sys.solve_dynamics()
 #%%
 
 plt.plot(r1.get_value(), r1.get_torque())
+
+# torque = inertie x acc = inertie x 2 x 2pi / t_simu^2 = 3 x 2 x 2pi / 5^2 = 1.50...
